84c0febbe7cc2f013f6ed050f32f039a2b6c5590
Nino Londaridze <nino_londaridze@epam.com>
2024-06-01 22:50:27 +0400

    deleted doubled folder

--- module_5/.mocharc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  spec: "./specs/*spec.js",
-  require: "chai/register-expect.js",
-  reporter: "mochawesome"
-}
--- module_5/config/endpoints.js
+++
@@ -1,3 +0,0 @@
-const TEST_URL = "https://jsonplaceholder.typicode.com/";
-
-module.exports = {TEST_URL};
